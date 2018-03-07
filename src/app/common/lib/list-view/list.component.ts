f5fff924dd9e0b757d9ff8e46bcab33d6452f237
shyave <shyave.99@gmail.com>
2018-03-07 17:01:53 +0530

    creating libs

---
+++ src/app/common/lib/list-view/list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-list-view',
+    templateUrl: 'list.component.html',
+    styles: ['list.component.css']
+})
+
+export class ListComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
